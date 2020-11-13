c11e7f14938fba88917ee49cb0e6be8478ef1015
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-11-13 05:23:07 +0300

    Dispatch an Action From Reducer

--- src/App.js
+++ src/App.js
@@ -7,13 +7,17 @@ const initalState = {
   count: 0
 };
 
-function reducer() {
-  console.log("reducer");
+// take current state and action
+function reducer(state = initalState, action) {
+  console.log("reducer", action);
   return initalState;
 }
 
 const store = createStore(reducer);
 console.log("Connected to store: ", store.getState());
+
+// reducer is called with new action
+store.dispatch({ type: "INCREMENT" });
 
 export default function App() {
   return (
