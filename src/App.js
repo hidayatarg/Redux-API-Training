a0ea2657dbd1b00d51b553a0f0507bfcc728616d
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-11-13 05:37:26 +0300

    Moved The Redux Code From App.js To Index.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,6 @@
 import React from "react";
 import "./styles.css";
 import Counter from "./Counter";
-import { createStore } from "redux";
-
-const initalState = {
-  count: 0
-};
-
-// take current state and action
-function reducer(state = initalState, action) {
-  console.log("reducer", action);
-  switch (action.type) {
-    case "INCREMENT":
-      return {
-        count: state.count + 1
-      };
-
-    case "DECREMENT":
-      return {
-        count: state.count - 1
-      };
-
-    default:
-      return state;
-  }
-}
-
-const store = createStore(reducer);
-console.log("Connected to store: ", store.getState());
-
-// reducer is called with new action
-store.dispatch({ type: "INCREMENT" });
-console.log("Connected to store: ", store.getState());
-
-store.dispatch({ type: "INCREMENT" });
-store.dispatch({ type: "DECREMENT" });
-console.log("Connected to store: ", store.getState());
 
 export default function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -3,6 +3,42 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 
+import { createStore } from "redux";
+
+const initalState = {
+  count: 0
+};
+
+// take current state and action
+function reducer(state = initalState, action) {
+  console.log("reducer", action);
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      };
+
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      };
+
+    default:
+      return state;
+  }
+}
+
+const store = createStore(reducer);
+console.log("Connected to store: ", store.getState());
+
+// reducer is called with new action
+store.dispatch({ type: "INCREMENT" });
+console.log("Connected to store: ", store.getState());
+
+store.dispatch({ type: "INCREMENT" });
+store.dispatch({ type: "DECREMENT" });
+console.log("Connected to store: ", store.getState());
+
 const rootElement = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
