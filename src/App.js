a95ebb6aa3f192fae7f2ee8ff8cdc887baae7900
Hidayat Arghandabi <hidayatarg@gmail.com>
2020-11-13 05:19:48 +0300

    Created Store and Reducers

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
 import React from "react";
 import "./styles.css";
 import Counter from "./Counter";
+import { createStore } from "redux";
+
+const initalState = {
+  count: 0
+};
+
+function reducer() {
+  console.log("reducer");
+  return initalState;
+}
+
+const store = createStore(reducer);
+console.log("Connected to store: ", store.getState());
 
 export default function App() {
   return (
